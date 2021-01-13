e2e8f50a263cb8a603f71e595fe1640603548537
Serg22 <neomorgan2286@gmail.com>
2021-01-13 08:15:17 +0300

    Variables

---
+++ Lesson_1/Variable.java
@@ -0,0 +1,20 @@
+public class Variable{
+	public static void main(String[] args) {
+		byte num_core = 4;
+		short cpu_frequency = 3300;
+		int disk_space = 4000000;
+		long ram = 64000;
+		float sectors = 25;
+		double power_supply = 750;
+		char effency_power = 'A';
+		boolean laptop = true;
+		System.out.println("Number core: " + num_core);
+		System.out.println("CPU frequency: " + cpu_frequency);
+		System.out.println("Disk space: " + disk_space);
+		System.out.println("Ram: " + ram);
+		System.out.println("Sectors: " + sectors);
+		System.out.println("Power supply: " + power_supply);
+		System.out.println(effency_power);
+		System.out.println("Laptop: " + laptop);		
+	}
+}
