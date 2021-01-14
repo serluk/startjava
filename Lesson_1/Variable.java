ed6272af2bf3fd763eab1e7390cb8a2606c132ef
Serg22 <neomorgan2286@gmail.com>
2021-01-14 15:09:46 +0300

    Fix variable name

--- Lesson_1/Variable.java
+++ Lesson_1/Variable.java
@@ -9,13 +9,13 @@ public class Variable {
         char effencyPower = 'A';
         boolean laptop = true;
         
-        System.out.println("Number core: " + num_core);
-        System.out.println("CPU frequency: " + cpu_frequency);
-        System.out.println("Disk space: " + disk_space);
+        System.out.println("Number core: " + numCore);
+        System.out.println("CPU frequency: " + cpuFrequency);
+        System.out.println("Disk space: " + diskSpace);
         System.out.println("Ram: " + ram);
         System.out.println("Sectors: " + sectors);
-        System.out.println("Power supply: " + power_supply);
-        System.out.println(effency_power);
+        System.out.println("Power supply: " + powerSupply);
+        System.out.println(effencyPower);
         System.out.println("Laptop: " + laptop);        
     }
 }
