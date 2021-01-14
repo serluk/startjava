8b3e1c3b2c45bc90173ee1f91e9e8e0a026ff962
Serg22 <neomorgan2286@gmail.com>
2021-01-14 07:52:11 +0300

    Variable

--- Lesson_1/Variable.java
+++ Lesson_1/Variable.java
@@ -1,20 +1,21 @@
-public class Variable{
-	public static void main(String[] args) {
-		byte num_core = 4;
-		short cpu_frequency = 3300;
-		int disk_space = 4000000;
-		long ram = 64000;
-		float sectors = 25;
-		double power_supply = 750;
-		char effency_power = 'A';
-		boolean laptop = true;
-		System.out.println("Number core: " + num_core);
-		System.out.println("CPU frequency: " + cpu_frequency);
-		System.out.println("Disk space: " + disk_space);
-		System.out.println("Ram: " + ram);
-		System.out.println("Sectors: " + sectors);
-		System.out.println("Power supply: " + power_supply);
-		System.out.println(effency_power);
-		System.out.println("Laptop: " + laptop);		
-	}
+public class Variable {
+    public static void main(String[] args) {
+        byte numCore = 4;
+        short cpuFrequency = 3300;
+        int diskSpace = 4000000;
+        long ram = 64000;
+        float sectors = 25;
+        double powerSupply = 750;
+        char effencyPower = 'A';
+        boolean laptop = true;
+        
+        System.out.println("Number core: " + num_core);
+        System.out.println("CPU frequency: " + cpu_frequency);
+        System.out.println("Disk space: " + disk_space);
+        System.out.println("Ram: " + ram);
+        System.out.println("Sectors: " + sectors);
+        System.out.println("Power supply: " + power_supply);
+        System.out.println(effency_power);
+        System.out.println("Laptop: " + laptop);        
+    }
 }
