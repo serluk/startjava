dd0fd45befb6ba58f5b312104888e5e61f38e9a8
Serg22 <neomorgan2286@gmail.com>
2021-01-15 10:07:13 +0300

    Fix exponentiation

--- Lesson_1/Calculator.java
+++ Lesson_1/Calculator.java
@@ -1,8 +1,9 @@
 public class Calculator {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         int num1 = 3;
         int num2 = 6;
-        char operation = '*';
+        char operation = '^';
+        
         if (operation == '+') {
             System.out.println(num1 + num2);
         } else if (operation == '-') {
@@ -15,12 +16,11 @@ public class Calculator {
             System.out.println(num1 % num2);
         } else if (operation == '^') {
             int count = 0;
-            int prod = 1;
-            do {
-                prod *= num1;
-                count++;
-            } while (count < num2);
-            System.out.println(prod);
+            int result = 1;
+            for(int i = 0; i < num2; i++) {
+                result *= num1;
+            }
+            System.out.println(result);
         }
     }
 }
