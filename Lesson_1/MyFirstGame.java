af4e658fc936b88ad1dfbaa5f3eb513b5ad39a12
serluk <neomorgan2286@gmail.com>
2021-01-17 16:47:27 +0300

    Using only two variables in MyFirstGame

--- Lesson_1/MyFirstGame.java
+++ Lesson_1/MyFirstGame.java
@@ -1,27 +1,21 @@
 public class MyFirstGame {
     public static void main(String[] args){
         int hiddenNumber = 10;
-        int num1 = 5;
-        int num2 = 21;
-        int num3 = 9;
-        int num4 = 10;
+        int num = 5;
+
         do {
-            if(hiddenNumber > num1) {
+            if(hiddenNumber > num) {
                 System.out.println("Заданное число болльше введенного");
             } else {
                 System.out.println("Заданное число меньше введенного");
             }
-            if(hiddenNumber > num2) {
-                System.out.println("Заданное число болльше введенного");
-            } else {
-                System.out.println("Заданное число меньше введенного");
+            num = num + 6;
+            if(hiddenNumber < num) {
+            	System.out.println("Заданное число меньше введенного");
             }
-            if(hiddenNumber > num3) {
-                System.out.println("Заданное число болльше введенного");
-            } else {
-                System.out.println("Заданное число меньше введенного");
-            }
-        }while (num4 != hiddenNumber);
+            num = num - 1;
+         
+        }while (num != hiddenNumber);
         System.out.println("Число угадано");
     }
 }
