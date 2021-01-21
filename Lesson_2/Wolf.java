2e8f3edeac978730cd2f78d66bd0900f3a8bc295
serluk <neomorgan2286@gmail.com>
2021-01-21 09:45:42 +0300

    Add classes Wolf and WolfTest

---
+++ Lesson_2/Wolf.java
@@ -0,0 +1,27 @@
+public class Wolf {
+    String sex;
+    String nickname;
+    int weight;
+    int age;
+    String color;
+
+    public void go(){
+        System.out.println("Go");
+    }
+
+    public void sit(){
+        System.out.println("Sit");
+    }
+
+    public void run(){
+        System.out.println("Run");
+    }
+
+    public void howl(){
+        System.out.println("Howl");
+    }
+
+    public void hunt(){
+        System.out.println("Hunt");
+    }
+}
---
+++ Lesson_2/WolfTest.java
@@ -0,0 +1,21 @@
+public class WolfTest {
+	public static void main(String[] args) {
+		Wolf wolf = new Wolf();
+		wolf.nickname = "Jack";
+		wolf.sex = "male";
+		wolf.weight = 30;
+		wolf.color = "grey";
+		wolf.age = 3;
+		System.out.println("Nickname: " + wolf.nickname);
+		System.out.println("Sex: " + wolf.sex);
+		System.out.println("Color: " + wolf.color);
+		System.out.println("Weight: " + wolf.weight);
+		System.out.println("Age: " + wolf.age);
+		wolf.go();
+		wolf.sit();
+		wolf.run();
+		wolf.howl();
+		wolf.hunt();
+
+	}
+}
