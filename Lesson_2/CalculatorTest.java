15456473876ce5d2b90fc8c7486c458347a6e3f4
Serg22 <neomorgan2286@gmail.com>
2021-01-22 12:04:27 +0300

    Calculator solution

--- Lesson_2/CalculatorTest.java
+++ Lesson_2/CalculatorTest.java
@@ -1,20 +1,18 @@
 import java.util.Scanner;
 
 public class CalculatorTest {
-    public static void main(String[] args) {
+    public static void main(String[] args){
         Calculator calculator = new Calculator();
         Scanner s = new Scanner(System.in);
-        int num1 = s.nextInt();
-        calculator.setNum1(num1);
-
-        String op = s.next();
-        calculator.setOp(op);
-
-        int num2 = s.nextInt();
-        calculator.setNum2(num2);
-
-        // System.out.println(calculator.getOp());
-        // System.out.println(calculator.getNum2());
-        System.out.println(calculator.result());
+        do {
+            int num1 = s.nextInt();
+            calculator.setNum1(num1);
+            String op = s.next();
+            calculator.setOp(op);
+            int num2 = s.nextInt();
+            calculator.setNum2(num2);
+            System.out.println(calculator.result());
+            System.out.println("Хотите продолжить вычисления? [да/нет]:");
+        } while (s.next() != "нет");
     }
 }
