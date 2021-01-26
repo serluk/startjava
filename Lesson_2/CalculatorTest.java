28cb3c2250c6c64afee33ad4a4d055705f68b047
serluk <neomorgan2286@gmail.com>
2021-01-26 22:46:00 +0300

    Fix Calculator

--- Lesson_2/CalculatorTest.java
+++ Lesson_2/CalculatorTest.java
@@ -3,19 +3,21 @@ import java.util.Scanner;
 public class CalculatorTest {
     public static void main(String[] args) {
         Calculator calculator = new Calculator();
+        String answer = "";
         Scanner scan = new Scanner(System.in);
+        System.out.print("Введите первое число: ");
+        int num1 = scan.nextInt();
+        calculator.setNum1(num1);
+        System.out.print("Введите операцию: ");
+        String sign = scan.next();
+        calculator.setSign(sign);
+        System.out.print("Введите второе число: ");
+        int num2 = scan.nextInt();
+        calculator.setNum2(num2);
+        System.out.println(calculator.result());
         do {
-            System.out.print("Введите первое число: ");
-            int num1 = scan.nextInt();
-            calculator.setNum1(num1);
-            System.out.print("Введите операцию: ");
-            String sign = scan.next();
-            calculator.setSign(sign);
-            System.out.print("Введите второе число: ");
-            int num2 = scan.nextInt();
-            calculator.setNum2(num2);
-            System.out.println(calculator.result());
-            System.out.println("Хотите продолжить вычисления? [да/нет]:");
-        } while (!scan.next().equals("нет"));
+            System.out.println("Хотите продолжить вычисления? [да/нет]:"); 
+            answer = scan.next();           
+        } while (!(answer.contains("нет"))||(!answer.contains("нет")));
     }
 }
